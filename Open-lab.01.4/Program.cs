6b7bfb315fce9aa73638082eb0d91a63451218f2
z18ista06 <z18ista06@Z00P1-17.SPSKNM>
2019-10-14 09:03:57 +0200

    Add project files.

---
+++ Open-lab.01.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_lab._01._4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var siteA = Console.ReadLine();
+            string height = Console.ReadLine();
+            int a = int.Parse(siteA);
+            int b = int.Parse(height);
+            int c = a * b / 2;
+            Console.WriteLine(c);
+        }
+    }
+}
